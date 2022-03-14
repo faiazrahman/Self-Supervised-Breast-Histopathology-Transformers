3f285b519f09ecb04446f0aff00f44da0ba641e5
faiazrahman <faiaz.rahman@yale.edu>
2022-03-14 16:43:50 -0400

    init model

--- model.py
+++ model.py
@@ -14,3 +14,51 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 logging.basicConfig(level=logging.INFO) # DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 print("CUDA available:", torch.cuda.is_available())
+
+class ConvolutionalNeuralNetModel(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, image, label):
+        pass
+
+class SelfSupervisedDinoTransformerModel(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, image, label):
+        pass
+
+class IDCDetectionModelTrainer(pl.LightningModule):
+
+    # TODO: Can we reuse this trainer for all the models?
+
+    def __init__(self, hparams=None):
+        pass
+
+    # Required for pl.LightningModule
+    def forward(self, image, label):
+        # pl.Lightning convention: forward() defines prediction for inference
+        return self.model(image, label)
+
+    # Required for pl.LightningModule
+    def training_step(self, batch, batch_idx):
+        pass
+
+    # Optional for pl.LightningModule
+    def training_step_end(self, batch_parts):
+        pass
+
+    # Optional for pl.LightningModule
+    def test_step(self, batch, batch_idx):
+        pass
+
+    # Optional for pl.LightningModule
+    def test_epoch_end(self, outputs):
+        pass
+
+    # Required for pl.LightningModule
+    def configure_optimizers(self):
+        pass
